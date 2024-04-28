072b34c2950ef4e7968f4952f4fa0f8bea776a47
Toropkin <mr.fobos96@mail.ru>
2024-04-28 16:02:45 +0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =  applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat1 = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Сравнение 2 пар бинов по ссылке, HelloWorld " + bean.equals(bean1));
+        System.out.println("Сравнение 2 пар бинов по ссылке, Cat " + cat.equals(cat1));
+        System.out.println(bean.getMessage());
+    }
+}
